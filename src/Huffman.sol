cc8a5a3782117dd73fdfaf2d98109ce4d6b4e344
malik672 <aremumalik01@gmail.com>
2023-06-09 06:44:03 +0100

    init

---
+++ src/Huffman.sol
@@ -0,0 +1,10 @@
+/// @title Huffman encoder and decoder in solidity adapted from https://github.com/bhrigu123/huffman-coding
+/// @notice SPDX-License-Identifier: MIT
+/// @author Malik_dev /      
+
+
+pragma solidity ^0.8.13;
+
+contract Huffman {
+  
+}
